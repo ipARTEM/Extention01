8de0c82508eea12e1fe1a5c070b2faccbf5243e1
Artem <a.khimin@yandex.ru>
2021-05-10 16:29:00 +0300

    Добавьте файлы проекта.

---
+++ Extention01/MyExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extention01_My
+{
+    static class MyExtensions
+    {
+        public static void Print(this DateTime current)
+        {
+            Console.WriteLine(current);
+        }
+
+        public static bool IsDayOfWeek(this DateTime dateTime, DayOfWeek dayOfWeek)
+        {
+            return dateTime.DayOfWeek == dayOfWeek;
+        }
+
+        public static string GetFullName(this Student student)
+        {
+            return student.LastName + " " + student.FirstName;
+        }
+
+    }
+}
---
+++ Extention01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Extention01_My;
+
+namespace Extention01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime currentDateTime = DateTime.Now;
+
+            currentDateTime.Print();
+
+            DateTime.Now.Print();
+
+            Console.WriteLine(currentDateTime.IsDayOfWeek(DayOfWeek.Thursday));
+
+            Student student = new Student() { FirstName = "Piter", LastName = "Falk" };
+
+            string fullName = student.GetFullName();
+
+        }
+    }
+}
